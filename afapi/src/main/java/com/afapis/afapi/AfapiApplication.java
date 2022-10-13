ae893a6d7a6a52396150ae701762470611b4494e
rameshkumard <rdhavil@gmail.com>
2022-10-13 08:38:10 -0500

    Initial commit

---
+++ afapi/src/main/java/com/afapis/afapi/AfapiApplication.java
@@ -0,0 +1,13 @@
+package com.afapis.afapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfapiApplication.class, args);
+	}
+
+}
---
+++ afapi/src/test/java/com/afapis/afapi/AfapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afapis.afapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
